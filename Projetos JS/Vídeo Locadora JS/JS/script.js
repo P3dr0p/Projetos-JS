37529d48344260dfc75c205519e11ce204bf0ff4
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-03-24 10:54:54 -0300

    Add files via upload

--- Projetos JS/Vídeo Locadora JS/JS/script.js
+++ Projetos JS/Vídeo Locadora JS/JS/script.js
@@ -1,12 +1,17 @@
-var inTitulo = document.getElementById("inTitulo");
-var inDuracao = document.getElementById("inDuracao");
-var outTitulo = document.getElementById("outTitulo");
-var outResposta = document.getElementById("outResposta")
+const inTitle = document.getElementById('inTitle');
+const inDuraction = document.getElementById('inDuraction');
+const outTitle = document.getElementById('outTitle');
+const outDuraction = document.getElementById('outDuraction');
+const buttonConverter = document.getElementById('btnConverter');
 
-var titulo = inTitulo.value;
-var duracao = Number(inDuracao.value);
-var horas = Math.floor(duracao / 60);
 
-outResposta.textContent = `${horas} hora(s) e ${minutos}minuto(s)`;
-var btConverter = document.getElenentById("btConverter");
-btConverter.addEventListener("click", converterDuracao);
+buttonConverter.addEventListener('click', () => {
+    let title = inTitle.ariaValueMax;
+    let duraction = Number(inDuraction.value);
+
+    let hours = Meth.floor(duraction / 60);
+    let minutes = duraction % 60;
+
+    outTitle.textContent = title;
+    outDuraction.textContent = `${hours} horas(s) e ${minutes} minuto(s)`;
+});
